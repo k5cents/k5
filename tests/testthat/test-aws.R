371f0931c615ee5476d3fcdd98d6d9a2d54f6e30
Kiernan Nicholls <kiernann@protonmail.com>
2020-11-12 16:44:24 -0500

    Update aws tests for ls

--- tests/testthat/test-aws.R
+++ tests/testthat/test-aws.R
@@ -1,10 +1,19 @@
-test_that("multiplication works", {
+test_that("bucket contents are listed as tibble", {
+  skip_on_cran()
+  skip_if_offline()
+  skip_if_not(nzchar(Sys.getenv("AWS_SECRET_ACCESS_KEY")))
+  z <- aws_info(bucket = "publicaccountability")
+  expect_length(z, 4)
+  expect_s3_class(z$path, "fs_path")
+  expect_s3_class(z$type, "factor")
+  expect_s3_class(z$size, "fs_bytes")
+  expect_s3_class(z$time, "POSIXt")
+})
+
+test_that("bucket objects are listed as vector", {
   skip_on_cran()
   skip_if_offline()
   skip_if_not(nzchar(Sys.getenv("AWS_SECRET_ACCESS_KEY")))
   z <- aws_ls(bucket = "publicaccountability")
-  expect_length(z, 3)
-  expect_s3_class(z$path, "fs_path")
-  expect_s3_class(z$size, "fs_bytes")
-  expect_s3_class(z$time, "POSIXt")
+  expect_s3_class(z, "fs_path")
 })
