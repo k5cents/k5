6c1d3ee85225be6dddd50212395c19b566b9edc1
Kiernan Nicholls <kiernann@protonmail.com>
2020-12-02 17:19:12 -0500

    Combine util functions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,15 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
 is_installed <- function(pkg) {
   isTRUE(requireNamespace(pkg, quietly = TRUE))
 }
