857ace44430bd7c449cd45f31e8bbc061724db4e
Kiernan Nicholls <kiernann@protonmail.com>
2022-03-22 10:52:15 -0400

    Add max default to `aws_info()`

--- R/aws.R
+++ R/aws.R
@@ -14,6 +14,7 @@
 #'   that can be retrieved with [aws_bucket()].
 #' @param prefix Character string that limits the response to keys that begin
 #'   with the specified prefix.
+#' @param max Number of objects to return.
 #' @param ... Additional arguments passed to [aws.s3::s3HTTP()].
 #' @examples
 #' aws_info("1000genomes", max = 10)
@@ -23,9 +24,9 @@
 #' @importFrom readr parse_datetime
 #' @importFrom tibble as_tibble
 #' @export
-aws_info <- function(bucket = aws_bucket(), prefix = NULL, ...) {
+aws_info <- function(bucket = aws_bucket(), prefix = NULL, max = Inf, ...) {
   stopifnot(!is.null(bucket))
-  z <- aws.s3::get_bucket_df(bucket = bucket, prefix = prefix, ...)
+  z <- aws.s3::get_bucket_df(bucket = bucket, prefix = prefix, max = max, ...)
   z$path <- fs::as_fs_path(z$Key)
   z$type <- ifelse(z$Size == 0, 2L, 5L)
   z$type <- factor(z$type, levels = file_types, labels = names(file_types))
