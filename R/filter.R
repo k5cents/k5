d399311284aa31ded9279bbe738b63c13d9aeec6
Kiernan Nicholls <kiernann@protonmail.com>
2022-04-13 15:55:21 -0400

    Add `filter_rx()`
    Import dplyr and stringr, which I already use

---
+++ R/filter.R
@@ -0,0 +1,15 @@
+#' Filter a data frame by a regular expression
+#'
+#' A shortcut for `dat %>% filter(str_detect(column, "\\d"))`.
+#'
+#' @param dat A data frame with a character column to filter.
+#' @param col The column containing a character vector to input.
+#' @param pattern Pattern to look for, passed to [stringr::str_detect()].
+#' @return A subset of rows from `dat`.
+#' @importFrom dplyr filter
+#' @importFrom stringr str_detect
+#' @export
+filter_rx <- function(dat, col, pattern) {
+  stopifnot(is.data.frame(dat))
+  dplyr::filter(dat, stringr::str_detect({{ col }}, pattern))
+}
