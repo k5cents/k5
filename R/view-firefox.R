8345c4469a058a6c88cb47fb7b361abd5c91bee2
Kiernan Nicholls <kiernann@protonmail.com>
2022-06-08 11:27:11 -0400

    Add `view_firefox()` utility

---
+++ R/view-firefox.R
@@ -0,0 +1,17 @@
+#' View an HTML document in Firefox
+#'
+#' Take an XML document opbject, write to an HTML file, and open in Firefox.
+#'
+#' @param html An object which has the class `xml_document`, often from rvest.
+#' @export
+view_firefox <- function(html) {
+  stopifnot(inherits(html, "xml_document"))
+  stopifnot(nzchar(Sys.which("firefox")))
+  tmp <- tempfile(fileext = ".html")
+  txt <- as.character(html)
+  writeLines(txt, tmp)
+  system2(
+    command = "firefox",
+    args = tmp
+  )
+}
