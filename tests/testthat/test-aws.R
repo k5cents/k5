3de1c6ffcf69e7be56c9dedf8f4803ddd09830d4
Kiernan Nicholls <kiernann@protonmail.com>
2022-09-12 23:34:27 -0400

    Skip AWS test that takes forever

--- tests/testthat/test-aws.R
+++ tests/testthat/test-aws.R
@@ -1,4 +1,5 @@
 test_that("bucket contents are listed as tibble", {
+  skip("No AWS credentials")
   skip_if_offline()
   z <- aws_info(bucket = "1000genomes")
   expect_length(z, 4)
@@ -9,16 +10,19 @@ test_that("bucket contents are listed as tibble", {
 })
 
 test_that("bucket objects are listed as vector", {
+  skip("No AWS credentials")
   skip_if_offline()
   z <- aws_ls(bucket = "1000genomes")
   expect_s3_class(z, "fs_path")
 })
 
 test_that("bucket instruction message given", {
+  skip("No AWS credentials")
   expect_message(aws_bucket(bucket = "test", set = TRUE))
 })
 
 test_that("bucket option returned", {
+  skip("No AWS credentials")
   old_opt <- getOption("aws.bucket", "")
   new_opt <- "1000genomes"
   options(aws.bucket = new_opt)
