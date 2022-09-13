739cf753dc484a9029803276c3f66f59aea4851f
Kiernan Nicholls <kiernann@protonmail.com>
2022-09-12 23:30:18 -0400

    Document `gaa` ID data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' GAA Team Abbreviations by Year
+#'
+#' @format A data frame with 64 rows and 3 variables:
+#' \describe{
+#'   \item{seasonId}{The fantasy season integer}
+#'   \item{teamId}{The team ID integer}
+#'   \item{abbrev}{The normalized _owner_ abbreviation for that year}
+#'   ...
+#' }
+"gaa"
