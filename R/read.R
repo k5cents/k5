fa6fd67c19e9d392de24b66d5e4d1f1cee47c07b
Kiernan Nicholls <kiernann@protonmail.com>
2022-12-17 20:03:47 -0500

    Add more dumb reading wrappers

--- R/read.R
+++ R/read.R
@@ -1,11 +1,25 @@
-#' Read a CSV file without column guessing
+#' Read a text file without column guessing
 #'
-#' Use [readr::read_csv()] without specifying _any_ column types. All columns
+#' Use [readr::read_delim()] without specifying _any_ column types. All columns
 #' are treated as character strings.
 #'
 #' @param file Either a path to a file, a connection, or literal data.
 #' @param ... Additional arguments passed to [readr::read_csv()].
-#' @importFrom readr read_csv cols
+#' @importFrom readr read_csv read_tsv read_delim cols
+#' @export
+read_delim_dumb <- function(file, delim = c(",", "\t", "|"), ...) {
+  readr::read_delim(
+    file = file,
+    delim = delim,
+    col_types = readr::cols(
+      .default = "c"
+    ),
+    guess_max = 0,
+    ...
+  )
+}
+
+#' @rdname read_delim_dumb
 #' @export
 read_csv_dumb <- function(file, ...) {
   readr::read_csv(
@@ -17,3 +31,18 @@ read_csv_dumb <- function(file, ...) {
     ...
   )
 }
+
+#' @rdname read_delim_dumb
+#' @export
+read_tsv_dumb <- function(file, ...) {
+  readr::read_tsv(
+    file = file,
+    col_types = readr::cols(
+      .default = "c"
+    ),
+    guess_max = 0,
+    ...
+  )
+}
+
+
