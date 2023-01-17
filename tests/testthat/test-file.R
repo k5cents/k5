bb27b50cf3a8c33920e71aac0f3ae393317010b1
Kiernan Nicholls <kiernann@protonmail.com>
2023-01-17 14:46:40 -0500

    Skip test on MacOS

--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -2,12 +2,14 @@ tmp <- tempfile()
 writeLines(LETTERS, tmp)
 
 test_that("new file has an age in a period class", {
+  skip_on_os("mac")
   skip_if_not(file.exists(tmp))
   a <- file_age(tmp)
   expect_true(inherits(a, "Period"))
 })
 
 test_that("new file has an age in a period class", {
+  skip_on_os("mac")
   skip_if_not(file.exists(tmp))
   e <- file_encoding(tmp)
   expect_s3_class(e, "data.frame")
