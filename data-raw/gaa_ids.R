f521d9d8bb8c587d8bb62fee278e1a0202f8ddec
Kiernan Nicholls <kiernann@protonmail.com>
2024-08-26 11:00:23 -0400

    Update `gaa` for 2024 season
    [skip ci]

--- data-raw/gaa_ids.R
+++ data-raw/gaa_ids.R
@@ -1,4 +1,13 @@
 ## code to prepare `DATASET` dataset goes here
+library(tidyverse)
+library(k5)
+
+gaa_2024 <- gaa %>%
+  filter(seasonId == 2023) %>%
+  mutate(
+    seasonId = 2024,
+    abbrev = fct_recode(abbrev, "JUST" = "CHAR")
+  )
 
 usethis::use_data(gaa, overwrite = TRUE)
 readr::write_csv(gaa, "data-raw/gaa_ids.csv", na = "")
